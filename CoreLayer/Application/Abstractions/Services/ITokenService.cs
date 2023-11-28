e0801fddfabea8b0fa61255127d55549c7050a37
MuammerKrc <muammer03karaca@gmail.com>
2023-11-29 01:31:15 +0300

    added base Identitiy Models and token models

---
+++ CoreLayer/Application/Abstractions/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.IdentityEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.AuthEntities;
+
+namespace Application.Abstractions.Services
+{
+	public interface ITokenService
+	{
+		JWTokens CreateAccessToken(AppUser appUser);
+
+	}
+}
---
+++ Infrastructures/Infrastructure/Services/TokenService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions.Services;
+using Domain.AuthEntities;
+using Domain.IdentityEntities;
+
+namespace Infrastructure.Services
+{
+	public class TokenService : ITokenService
+	{
+		public JWTokens CreateAccessToken(AppUser appUser)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
