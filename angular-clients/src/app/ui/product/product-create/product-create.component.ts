470d6e11960f6a8a626d98353dede0230fc8c3e9
MuammerKrc <muammer03karaca@gmail.com>
2023-12-21 21:47:25 +0300

    product create component developed on Angular clients

--- angular-clients/src/app/ui/product/product-create/product-create.component.ts
+++ angular-clients/src/app/ui/product/product-create/product-create.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductDto, ProductService } from 'src/generated_endpoints';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent  {
+  product:ProductDto={};
+  constructor(private productService:ProductService){
+    }
+    @Output() createdProductEvent :EventEmitter<any>=new EventEmitter<ProductDto>;
+    
+  onSubmit($event:MouseEvent,txtName:HTMLInputElement,txtStock:HTMLInputElement,txtPrice:HTMLInputElement){
+    $event.preventDefault();
+    this.product.name=txtName.value;
+    this.product.price=Number.parseFloat(txtPrice.value);
+    this.product.stock=Number.parseInt(txtStock.value);
+    this.productService.productCreateProductPost(this.product).subscribe(p=>{
+      this.createdProductEvent.emit(this.product);
+    });
+  }
 
 }
--- angular-clients/src/app/ui/product/product.component.ts
+++ angular-clients/src/app/ui/product/product.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/generated_endpoints';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductDto, ProductService } from 'src/generated_endpoints';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +8,17 @@ import { ProductService } from 'src/generated_endpoints';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent  implements OnInit {
+
+  @ViewChild(ProductListComponent) productListViewChild:ProductListComponent;
+
   constructor(private productService:ProductService){}
   ngOnInit(): void {
     this.productService.productGetAllProdutsGet().subscribe(x=>{
       console.log(x);
     })
+  }
+  createdProductfunc(product:ProductDto){
+    this.productListViewChild.getProduct();
+
   }
 }
